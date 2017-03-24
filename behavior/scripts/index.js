5447164c2272422cee727781e6932604cad20ed6
Christopher Desmarais <christopherdesmarais@ChristophersMBP.usny.ibm.com>
2017-03-24 15:39:37 -0400

    changed logic

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -2,25 +2,6 @@
 
 exports.handle = (client) => {
   // Create steps
-  const sayHello = client.createStep({
-    satisfied() {
-      return Boolean(client.getConversationState().helloSent)
-    },
-
-    prompt() {
-      client.addResponse('welcome')
-      client.addResponse('provide/documentation', {
-        documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('provide/instructions')
-
-      client.updateConversationState({
-        helloSent: true
-      })
-
-      client.done()
-    }
-  })
 
   const untrained = client.createStep({
     satisfied() {
@@ -33,7 +14,41 @@ exports.handle = (client) => {
     }
   })
 
-  client.runFlow({
+    const collectCity = client.createStep({
+      satisfied() {
+        return Boolean(client.getConversationState().weatherCity)
+      },
+        
+      extractInfo() {
+        const city = client.getFirstEntityWithRole(client.getMessagePart(), 'city')
+        
+        if (city) {
+          client.updateConversationState({
+            weatherCity: city,
+          })
+          
+          console.log('User wants the weather in:', city.value)
+        }
+      },
+
+      prompt() {
+        client.addResponse('prompt/weather_city')
+        client.done()
+      },
+    })
+
+    const provideWeather = client.createStep({
+      satisfied() {
+        return false
+      },
+
+      prompt() {
+        // Need to provide weather
+        client.done()
+      },
+    })
+    
+    client.runFlow({
     classifications: {
       // map inbound message classifications to names of streams
     },
@@ -41,9 +56,10 @@ exports.handle = (client) => {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
     streams: {
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained],
+      main: 'getWeather',
+      hi: [sayHello],
+      getWeather: [collectCity, provideWeather],
     },
   })
+  
 }
